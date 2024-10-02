a59eee0c8d7583c715b9fbe1113fe5c58a9e8011
Lukas <lukaswp2710@gmail.com>
2024-10-02 14:12:35 +0200

    Includes footer og nav

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<div class="container-fluid bg-primary fixed-bottom">
+    <nav class="navbar navbar-expand-lg d-flex justify-content-around">
+        <div class="col-1 d-flex justify-content-center">
+            <a class="text-end" href="index.php"><i class="fa-solid fa-house text-secondary fs-1"></i></a>
+        </div>
+        <div class="col-1 d-flex justify-content-center">
+            <a class="text-end" href="profile.php"><i class="fa-solid fa-user text-secondary fs-1"></i></a>
+        </div>
+    </nav>
+</div>
---
+++ includes/nav.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg bg-primary ">
+    <div class="container-fluid d-flex justify-content-end">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav w-100 d-flex justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link text-secondary text-end" href="index.php">Forside</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-secondary text-end" href="matches.php">Kampe</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-secondary text-end" href="statistics.php">Statistikker</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-secondary text-end" href="profile.php">Profil</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
